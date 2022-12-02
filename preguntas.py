b14ab3504171370c02d93ffd71bb6d86060f5c22
SaraAlzateC <salzateco@unal.edu.co>
2022-12-01 22:05:18 -0500

    concrete 7

--- preguntas.py
+++ preguntas.py
@@ -116,7 +116,7 @@ def pregunta_04():
         'learning_rate': ['adaptative'],  
         'momentum': [0.7, 0.8, 0.9],  
         'learning_rate_init': [0.01, 0.05, 0.1],  
-        'max_iter': 5000,  
+        'max_iter': [5000],  
         'early_stopping': True,  
     }
 
